36cacbb04aae2a1a8d31d4ccc6fbe5e527579605
onsjawadi <jawadi.ons.97@gmail.com>
2019-06-18 18:33:44 +0200

    commit 2

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -1,40 +1,41 @@
 export default function() {
   return [
     {
-      title: "Blog Dashboard",
-      to: "/blog-overview",
-      htmlBefore: '<i class="material-icons">edit</i>',
+      title: "Client",
+      to: "/clients",
+      htmlBefore: '',
       htmlAfter: ""
     },
     {
-      title: "Blog Posts",
-      htmlBefore: '<i class="material-icons">vertical_split</i>',
-      to: "/blog-posts",
+      title: "Utilisateur",
+      htmlBefore: '',
+      to: "/Utilisateurs",
     },
     {
-      title: "Add New Post",
-      htmlBefore: '<i class="material-icons">note_add</i>',
-      to: "/add-new-post",
+      title: "Devis",
+      htmlBefore: '',
+      to: "/Devis",
     },
     {
-      title: "Forms & Components",
-      htmlBefore: '<i class="material-icons">view_module</i>',
-      to: "/components-overview",
+      title: "Stocks",
+      htmlBefore: '',
+      to: "/Stocks",
     },
     {
-      title: "Tables",
-      htmlBefore: '<i class="material-icons">table_chart</i>',
-      to: "/tables",
+      title: "Machines",
+      htmlBefore: '',
+      to: "/Machines",
     },
     {
-      title: "User Profile",
-      htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
+      title: "Offre De Prix",
+      htmlBefore: '',
+      to: "/OffresDePrix",
     },
+    ,
     {
-      title: "Errors",
-      htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
+      title: "Pieces",
+      htmlBefore: '',
+      to: "/Pieces",
     }
   ];
 }
